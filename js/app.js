a02cc7f7ed375d0461aed84a08c5a59ac49a6b51
Ashlee Brolly <ashleebrolly@Ashlees-MBP.attlocal.net>
2018-01-05 10:09:23 -0500

    commented out old code

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ function initMap() {
 
 } // now it IS a function and it is in global
 
-$(() => {
-    initMap = function () {
-        // your code like...
-        var map = new google.maps.Map(document.getElementById('map'), { ...});
-        // and other stuff...
-    }
-})
+// $(() => {
+//     initMap = function () {
+//         // your code like...
+//         var map = new google.maps.Map(document.getElementById('map'), { ...});
+//         // and other stuff...
+//     }
+// })
 
 
 $(document).ready(function () {
