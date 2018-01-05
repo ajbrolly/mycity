5ea8224bb0e64ba8f5ce18154a54448b84ed705f
Ashlee Brolly <ashleebrolly@Ashlees-MBP.attlocal.net>
2018-01-05 13:07:22 -0500

    fix

--- js/profile.js
+++ js/profile.js
@@ -1,174 +1,172 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-// var config = {
-//     apiKey: 'AIzaSyDMqNQ9pA7C5sKkMHm8U6BAdExqtprHAwE',
-//     authDomain: 'mycity-188015.firebaseapp.com',
-//     databaseURL: 'https://mycity-188015.firebaseio.com',
-//     projectId: 'mycity-188015',
-//     storageBucket: 'mycity-188015.appspot.com',
-//     messagingSenderId: '986949142496'
-// };
+    // var config = {
+    //     apiKey: 'AIzaSyDMqNQ9pA7C5sKkMHm8U6BAdExqtprHAwE',
+    //     authDomain: 'mycity-188015.firebaseapp.com',
+    //     databaseURL: 'https://mycity-188015.firebaseio.com',
+    //     projectId: 'mycity-188015',
+    //     storageBucket: 'mycity-188015.appspot.com',
+    //     messagingSenderId: '986949142496'
+    // };
 
-// firebase.initializeApp(config);
+    // firebase.initializeApp(config);
 
-// var database = firebase.database();
+    // var database = firebase.database();
 
-// var databaseKeys = [];
-var count = 0;
+    // var databaseKeys = [];
+    var count = 0;
 
-$('#map').hide();
-
-function logPlaceDetails(placeId) {
-    var service = new google.maps.places.PlacesService(document.getElementById('map'));
-    service.getDetails({
-        placeId: placeId
-    }, function (place) {
-        console.log('Place details:', place);
-        var icon;
-        var category;
-        if (place.types[0] === 'restaurant' || place.types[0] === 'meal_takeaway' || place.types[0] === 'bakery' || place.types[0] === 'food') {
-            icon = ('<span class="glyphicon glyphicon-cutlery" aria-hidden="true">' + '</span>');
-            category = 'Restaurant';
-        } else if (place.types[0] === 'bar') {
-            icon = ('<span class="glyphicon glyphicon-glass" aria-hidden="true">' + '</span>');
-            category = 'Bar';
-        } else if (place.types[0] === 'casino' || place.types[0] === 'bowling_alley' || place.types[0] === 'stadium' || place.types[0] === 'amusement_park' || place.types[0] === 'aquarium') {
-            icon = ('<span class="glyphicon glyphicon-sunglasses" aria-hidden="true">' + '</span>');
-            category = 'Entertainment';
-        } else if (place.types[0] === 'museum') {
-            icon = ('<span class="glyphicon glyphicon-knight" aria-hidden="true">' + '</span>');
-            category = 'Museum';
-        } else if (place.types[0] === 'shopping_mall' || place.types[0] === 'clothing_store') {
-            icon = ('<span class="glyphicon glyphicon-credit-card" aria-hidden="true">' + '</span>');
-            category = 'Shopping';
-        } else {
-            icon = ('<span class="glyphicon glyphicon-heart" aria-hidden="true">' + '</span>');
-            category = 'Favorite';
-        }
-        var panel = $('<div class="panel panel-default" id="place' + count + '">');
-        var div = $('<div class="panel-body">');
-        div.append('<strong>' + place.name + '</strong>' + '<br />');
-        div.append(place.adr_address + '<br />');
-        div.append('<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>' + ': ' + place.formatted_phone_number + '<br />');
-        div.append('<span class="glyphicon glyphicon-globe" aria-hidden="true"></span>' + ': ' + '<a href="' + place.website + '" target="_blank">' + 'Website' + '</a>' + '<br />');
-        div.append(icon + ': ' + category + '</br>');
-        var buttonGroup = $('<div class="btn-group" role="group" aria-label="...">')
-        var button = $('<button id="place-visited" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-ok" aria-hidden="true">' + '</span>' + ' Complete' + '</button>');
-        buttonGroup.append(button);
-        var deleteButton = $('<button id="delete-place" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-remove" aria-hidden="true">' + '</span>' + ' Delete' + '</button>');
-        buttonGroup.append(deleteButton);
-        div.append(buttonGroup);
-        panel.append(div);
-        $('#favorites').prepend(panel);
-        count++;
-    });
-};
-
-function logRecentDetails(placeId) {
-    var service = new google.maps.places.PlacesService(document.getElementById('map'));
-    service.getDetails({
-        placeId: placeId
-    }, function (place) {
-        console.log('Place details:', place);
-        var icon;
-        var category;
-        if (place.types[0] === 'restaurant' || place.types[0] === 'meal_takeaway' || place.types[0] === 'bakery' || place.types[0] === 'food') {
-            icon = ('<span class="glyphicon glyphicon-cutlery" aria-hidden="true">' + '</span>');
-            category = 'Restaurant';
-        } else if (place.types[0] === 'bar') {
-            icon = ('<span class="glyphicon glyphicon-glass" aria-hidden="true">' + '</span>');
-            category = 'Bar';
-        } else if (place.types[0] === 'casino' || place.types[0] === 'bowling_alley' || place.types[0] === 'stadium' || place.types[0] === 'amusement_park' || place.types[0] === 'aquarium') {
-            icon = ('<span class="glyphicon glyphicon-sunglasses" aria-hidden="true">' + '</span>');
-            category = 'Entertainment';
-        } else if (place.types[0] === 'museum') {
-            icon = ('<span class="glyphicon glyphicon-knight" aria-hidden="true">' + '</span>');
-            category = 'Museum';
-        } else if (place.types[0] === 'shopping_mall' || place.types[0] === 'clothing_store') {
-            icon = ('<span class="glyphicon glyphicon-credit-card" aria-hidden="true">' + '</span>');
-            category = 'Shopping';
-        } else {
-            icon = ('<span class="glyphicon glyphicon-heart" aria-hidden="true">' + '</span>');
-            category = 'Favorite';
-        }
-        var panel = $('<div class="panel panel-default" id="place' + count + '">');
-        var div = $('<div class="panel-body">');
-        div.append('<strong>' + place.name + '</strong>' + '<br />');
-        div.append(place.adr_address + '<br />');
-        div.append('<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>' + ': ' + place.formatted_phone_number + '<br />');
-        div.append('<span class="glyphicon glyphicon-globe" aria-hidden="true"></span>' + ': ' + '<a href="' + place.website + '" target="_blank">' + 'Website' + '</a>' + '<br />');
-        div.append(icon + ': ' + category + '</br>');
-        var deleteButton = $('<button id="delete-place" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-remove" aria-hidden="true">' + '</span>' + ' Delete' + '</button>');
-        div.append(deleteButton);
-        panel.append(div);
-        $('#recents').prepend(panel);
-        count++;
-    });
-};
-
-function placeVisited() {
-    var placeValue = ($(this).attr('data'));
-    $(this).remove();
-    $('#place' + placeValue).detach('#favorites');
-    $('#recents').prepend($('#place' + placeValue));
-    database.ref(databaseKeys[placeValue]).update({
-        recent: true
-    });
-};
-
-function deletePlace() {
-    var placeValue = ($(this).attr('data'));
-    $('#place' + placeValue).remove();
-    database.ref(databaseKeys[placeValue]).remove();
-};
-
-$(document).on('click', '#place-visited', placeVisited);
-$(document).on('click', '#delete-place', deletePlace);
-
-$('#map').hide();
-$('#profile-page').hide();
-
-//Code to sign in with Google profile via Firebase
-var user;
-var uid;
-
-function toggleSignIn() {
-    if (!firebase.auth().currentUser) {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-            var token = result.credential.accessToken;
-            user = result.user;
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-            if (errorCode === 'auth/account-exists-with-different-credential') {
-                console.log('User has already signed up with a different auth provider for that email.');
+    function logPlaceDetails(placeId) {
+        var service = new google.maps.places.PlacesService(document.getElementById('map'));
+        service.getDetails({
+            placeId: placeId
+        }, function (place) {
+            console.log('Place details:', place);
+            var icon;
+            var category;
+            if (place.types[0] === 'restaurant' || place.types[0] === 'meal_takeaway' || place.types[0] === 'bakery' || place.types[0] === 'food') {
+                icon = ('<span class="glyphicon glyphicon-cutlery" aria-hidden="true">' + '</span>');
+                category = 'Restaurant';
+            } else if (place.types[0] === 'bar') {
+                icon = ('<span class="glyphicon glyphicon-glass" aria-hidden="true">' + '</span>');
+                category = 'Bar';
+            } else if (place.types[0] === 'casino' || place.types[0] === 'bowling_alley' || place.types[0] === 'stadium' || place.types[0] === 'amusement_park' || place.types[0] === 'aquarium') {
+                icon = ('<span class="glyphicon glyphicon-sunglasses" aria-hidden="true">' + '</span>');
+                category = 'Entertainment';
+            } else if (place.types[0] === 'museum') {
+                icon = ('<span class="glyphicon glyphicon-knight" aria-hidden="true">' + '</span>');
+                category = 'Museum';
+            } else if (place.types[0] === 'shopping_mall' || place.types[0] === 'clothing_store') {
+                icon = ('<span class="glyphicon glyphicon-credit-card" aria-hidden="true">' + '</span>');
+                category = 'Shopping';
             } else {
-                console.error(error);
+                icon = ('<span class="glyphicon glyphicon-heart" aria-hidden="true">' + '</span>');
+                category = 'Favorite';
             }
+            var panel = $('<div class="panel panel-default" id="place' + count + '">');
+            var div = $('<div class="panel-body">');
+            div.append('<strong>' + place.name + '</strong>' + '<br />');
+            div.append(place.adr_address + '<br />');
+            div.append('<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>' + ': ' + place.formatted_phone_number + '<br />');
+            div.append('<span class="glyphicon glyphicon-globe" aria-hidden="true"></span>' + ': ' + '<a href="' + place.website + '" target="_blank">' + 'Website' + '</a>' + '<br />');
+            div.append(icon + ': ' + category + '</br>');
+            var buttonGroup = $('<div class="btn-group" role="group" aria-label="...">')
+            var button = $('<button id="place-visited" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-ok" aria-hidden="true">' + '</span>' + ' Complete' + '</button>');
+            buttonGroup.append(button);
+            var deleteButton = $('<button id="delete-place" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-remove" aria-hidden="true">' + '</span>' + ' Delete' + '</button>');
+            buttonGroup.append(deleteButton);
+            div.append(buttonGroup);
+            panel.append(div);
+            $('#favorites').prepend(panel);
+            count++;
         });
-    } else {
-        firebase.auth().signOut();
-    }
-    document.getElementById('quickstart-sign-in').disabled = true;
-};
-
-// function initApp() {
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        console.log(user);
-        uid = user.uid;
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-    } else {
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-    }
-    document.getElementById('quickstart-sign-in').disabled = false;
-});
-document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
     };
+
+    function logRecentDetails(placeId) {
+        var service = new google.maps.places.PlacesService(document.getElementById('map'));
+        service.getDetails({
+            placeId: placeId
+        }, function (place) {
+            console.log('Place details:', place);
+            var icon;
+            var category;
+            if (place.types[0] === 'restaurant' || place.types[0] === 'meal_takeaway' || place.types[0] === 'bakery' || place.types[0] === 'food') {
+                icon = ('<span class="glyphicon glyphicon-cutlery" aria-hidden="true">' + '</span>');
+                category = 'Restaurant';
+            } else if (place.types[0] === 'bar') {
+                icon = ('<span class="glyphicon glyphicon-glass" aria-hidden="true">' + '</span>');
+                category = 'Bar';
+            } else if (place.types[0] === 'casino' || place.types[0] === 'bowling_alley' || place.types[0] === 'stadium' || place.types[0] === 'amusement_park' || place.types[0] === 'aquarium') {
+                icon = ('<span class="glyphicon glyphicon-sunglasses" aria-hidden="true">' + '</span>');
+                category = 'Entertainment';
+            } else if (place.types[0] === 'museum') {
+                icon = ('<span class="glyphicon glyphicon-knight" aria-hidden="true">' + '</span>');
+                category = 'Museum';
+            } else if (place.types[0] === 'shopping_mall' || place.types[0] === 'clothing_store') {
+                icon = ('<span class="glyphicon glyphicon-credit-card" aria-hidden="true">' + '</span>');
+                category = 'Shopping';
+            } else {
+                icon = ('<span class="glyphicon glyphicon-heart" aria-hidden="true">' + '</span>');
+                category = 'Favorite';
+            }
+            var panel = $('<div class="panel panel-default" id="place' + count + '">');
+            var div = $('<div class="panel-body">');
+            div.append('<strong>' + place.name + '</strong>' + '<br />');
+            div.append(place.adr_address + '<br />');
+            div.append('<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>' + ': ' + place.formatted_phone_number + '<br />');
+            div.append('<span class="glyphicon glyphicon-globe" aria-hidden="true"></span>' + ': ' + '<a href="' + place.website + '" target="_blank">' + 'Website' + '</a>' + '<br />');
+            div.append(icon + ': ' + category + '</br>');
+            var deleteButton = $('<button id="delete-place" data="' + count + '" type="button" class="btn btn-default btn-sm">' + '<span class="glyphicon glyphicon-remove" aria-hidden="true">' + '</span>' + ' Delete' + '</button>');
+            div.append(deleteButton);
+            panel.append(div);
+            $('#recents').prepend(panel);
+            count++;
+        });
+    };
+
+    function placeVisited() {
+        var placeValue = ($(this).attr('data'));
+        $(this).remove();
+        $('#place' + placeValue).detach('#favorites');
+        $('#recents').prepend($('#place' + placeValue));
+        database.ref(databaseKeys[placeValue]).update({
+            recent: true
+        });
+    };
+
+    function deletePlace() {
+        var placeValue = ($(this).attr('data'));
+        $('#place' + placeValue).remove();
+        database.ref(databaseKeys[placeValue]).remove();
+    };
+
+    $(document).on('click', '#place-visited', placeVisited);
+    $(document).on('click', '#delete-place', deletePlace);
+
+    $('#map').hide();
+    $('#profile-page').hide();
+
+    //Code to sign in with Google profile via Firebase
+    var user;
+    var uid;
+
+    function toggleSignIn() {
+        if (!firebase.auth().currentUser) {
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                var token = result.credential.accessToken;
+                user = result.user;
+            }).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                if (errorCode === 'auth/account-exists-with-different-credential') {
+                    console.log('User has already signed up with a different auth provider for that email.');
+                } else {
+                    console.error(error);
+                }
+            });
+        } else {
+            firebase.auth().signOut();
+        }
+        document.getElementById('quickstart-sign-in').disabled = true;
+    };
+
+    // function initApp() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log(user);
+            uid = user.uid;
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+        } else {
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+        }
+        document.getElementById('quickstart-sign-in').disabled = false;
+    });
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+};
 
 window.onload = function () {
     initApp();
@@ -237,4 +235,4 @@ function upcomingEvents() {
 };
 
 
-// });
+});
